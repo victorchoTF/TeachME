1ed6749bcb7b40b6d4d5124f3118bbc76495ad81
Victor Georgiev <vrgeorgiev06@gmail.com>
2025-01-19 14:52:40 +0200

    initial commit

---
+++ TeachME/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TeachME
+//
+//  Created by TumbaDev on 19.01.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TeachME/TeachMEApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeachMEApp.swift
+//  TeachME
+//
+//  Created by TumbaDev on 19.01.25.
+//
+
+import SwiftUI
+
+@main
+struct TeachMEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
