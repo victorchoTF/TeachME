46ba1e1e33cc82a7cf501808a4e5e3441fdc2f97
Victor Georgiev <vrgeorgiev06@gmail.com>
2025-03-08 16:43:17 +0200

    Added base implementation via extension to Repository protocol

--- TeachME/DataLayer/Repositories/Repository.swift
+++ TeachME/DataLayer/Repositories/Repository.swift
@@ -9,12 +9,54 @@ import Foundation
 
 protocol Repository {
     associatedtype ModelType: Model
+    associatedtype MapperType: Mapper where MapperType.ModelType == ModelType
+    associatedtype DataSource: APIDataSource where DataSource.DataType == MapperType.DataType
+
     
-    var dataSource: any APIDataSource { get }
+    var dataSource: DataSource { get }
+    var mapper: MapperType { get }
     
     func create(_ model: ModelType) async throws -> ModelType
     func getById(_ id: UUID) async throws -> ModelType
     func update(_ model: ModelType) async throws
     func delete(_ id: UUID) async throws
     func getAll() async throws -> [ModelType]
+}
+
+extension Repository {
+    func create(_ model: ModelType) async throws -> ModelType {
+        let data = mapper.modelToData(model)
+        
+        let result = try await dataSource.create(data)
+        
+        return mapper.dataToModel(result)
+    }
+    
+    func getById(_ id: UUID) async throws -> ModelType {
+        let result = try await dataSource.fetchById(id)
+        
+        return mapper.dataToModel(result)
+    }
+    
+    func update(_ model: ModelType) async throws {
+        let data = mapper.modelToData(model)
+        
+        try await dataSource.update(data)
+    }
+    
+    func delete(_ id: UUID) async throws {
+        try await dataSource.delete(id)
+    }
+    
+    func getAll() async throws -> [ModelType] {
+        let dataList = try await dataSource.fetchAll()
+        
+        var result: [ModelType] = []
+        
+        for data in dataList {
+            result.append(mapper.dataToModel(data))
+        }
+        
+        return result
+    }
 }
